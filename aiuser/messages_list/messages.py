80603a9941b16dbd3b6f3a79fc4a3dc3d2c98f81
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2024-05-23 22:06:15 -0400

    Revert "log"
    This reverts commit 4124a851d24b53f6a185fdc5a1fd55d9432e3bf7.

--- aiuser/messages_list/messages.py
+++ aiuser/messages_list/messages.py
@@ -250,7 +250,7 @@ class MessagesList:
             entry = MessageEntry("assistant", self.prefill)
             messages.append(entry)
         jsn = [asdict(message) for message in messages]
-        logger.info(jsn)
+        # logger.debug(jsn)
         return jsn
 
     async def _add_tokens(self, content):
