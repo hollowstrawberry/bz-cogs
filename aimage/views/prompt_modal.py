ea0603099eeb1fce7d152427019c870982c4a016
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-09-02 20:13:07 -0400

    title

--- aimage/views/prompt_modal.py
+++ aimage/views/prompt_modal.py
@@ -5,10 +5,9 @@ from aimage.views.image_actions import ImageActions
 
 class PromptModal(discord.ui.Modal):
     def __init__(self, parent: ImageActions, interaction: discord.Interaction, prompt: str, negative_prompt: str):
-        super().__init__()
+        super().__init__(title="Modify Prompt")
         self.src_view = parent
         self.src_interaction = interaction
-        self.title = "Modify Prompt"
         self.add_item(discord.ui.TextInput(label="Prompt", default=prompt, placeholder=prompt, min_length=4))
         self.add_item(discord.ui.TextInput(label="Negative Prompt", default=negative_prompt, placeholder=negative_prompt, min_length=4))
 
